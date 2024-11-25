6f543de3ea1ae0fa5f5fdadecb6c56a0479e1db6
venkatesh <mdrvenky@gmail.com>
2024-11-25 16:18:54 +0530

    NewBean Commit

--- GitDemo/src/com/botree/Main.java
+++ GitDemo/src/com/botree/Main.java
@@ -5,6 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("hello  from main while commiting by vbranch");
+		
+		System.out.println("A new Commit");
 	}
 
 }
